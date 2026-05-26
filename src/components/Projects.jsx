4c8faa03c6121c59847c0cf1b24fbb56c7ffa724
ricemb05 <ricembotones05@gmail.com>
2026-05-26 15:49:44 +0800

    updates for project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,11 @@ import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+import data from "../assets/data/projects.json"
+
+
+
+
 gsap.registerPlugin(ScrollTrigger);
 
 
@@ -26,6 +31,7 @@ export default function Projects() {
                     scrub: 1,
                     pin: true,
                     anticipatePin: 1,
+
                     // markers: true,
                 }
             });
@@ -33,8 +39,14 @@ export default function Projects() {
             // Phase 1: entrance (small portion of scroll)
             tl.fromTo(
                 track,
-                { y: 120, opacity: 0 },
-                { y: 0, opacity: 1, ease: "none", duration: 0.15 }
+                {
+                    clipPath: "inset(50% 0% 50% 0%)",
+                },
+                {
+                    clipPath: "inset(0% 0% 0% 0%)",
+                    duration: 0.6,
+                    ease: "power3.out"
+                }
             )
 
                 // Phase 2: horizontal scroll (main part)
@@ -70,7 +82,48 @@ export default function Projects() {
 
                 <section className="horizontal-section" ref={sectionRef}>
                     <div className="horizontal-track" ref={trackRef}>
-                        <div className="panel">
+                        {data.projects.map((project) => (
+                            <div className="panel" key={project.id}>
+                                <div className="project-container">
+                                    <div className="first-section">
+                                        <h2>{project.title}</h2>
+                                    </div>
+                                    <div className="second-section">
+                                        <div className="project-preview">
+                                            <div className="image-pannel first">
+                                                <img className="image-1" src={project.images.image[0]} alt={project.images.alts[0]} />
+                                            </div>
+                                            <div className="image-pannel second">
+                                                <img className="image-2" src={project.images.image[1]} alt={project.images.alts[1]} />
+                                                <img className="image-3" src={project.images.image[2]} alt={project.images.alts[2]} />
+                                            </div>
+                                        </div>
+                                        <div className="project-details">
+                                            <div className="project-description">
+                                                <p>
+                                                    {project.description}
+                                                </p>
+                                            </div>
+                                            <div className="project-technologies">
+                                                <div className="technologies">
+                                                    {project.techStack.map((tech, i) => (
+                                                        <span key={i} className="tech-badge">
+                                                            {tech}
+                                                        </span>
+                                                    ))}
+                                                </div>
+                                                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                                                    View now
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+
+
+                        {/* <div className="panel">
                             <div className="project-container">
                                 <div className="first-section">
                                     <h2>Project Title</h2>
@@ -97,91 +150,7 @@ export default function Projects() {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="panel">
-                            <div className="project-container">
-                                <div className="first-section">
-                                    <h2>Project Title</h2>
-                                </div>
-                                <div className="second-section">
-                                    <div className="project-preview">
-
-                                    </div>
-                                    <div className="project-details">
-                                        <div className="project-description">
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                            </p>
-                                        </div>
-                                        <div className="project-technologies">
-                                            <div className="technologies">
-                                                <span>HTML</span>
-                                                <span>CSS</span>
-                                                <span>JAVSCRIPT</span>
-                                                <span>PHP</span>
-                                            </div>
-                                            <button>View now</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="panel">
-                            <div className="project-container">
-                                <div className="first-section">
-                                    <h2>Project Title</h2>
-                                </div>
-                                <div className="second-section">
-                                    <div className="project-preview">
-
-                                    </div>
-                                    <div className="project-details">
-                                        <div className="project-description">
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                            </p>
-                                        </div>
-                                        <div className="project-technologies">
-                                            <div className="technologies">
-                                                <span>HTML</span>
-                                                <span>CSS</span>
-                                                <span>JAVSCRIPT</span>
-                                                <span>PHP</span>
-                                            </div>
-                                            <button>View now</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="panel">
-                            <div className="project-container">
-                                <div className="first-section">
-                                    <h2>Project Title</h2>
-                                </div>
-                                <div className="second-section">
-                                    <div className="project-preview">
-
-                                    </div>
-                                    <div className="project-details">
-                                        <div className="project-description">
-                                            <p>
-                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                            </p>
-                                        </div>
-                                        <div className="project-technologies">
-                                            <div className="technologies">
-                                                <span>HTML</span>
-                                                <span>CSS</span>
-                                                <span>JAVSCRIPT</span>
-                                                <span>PHP</span>
-                                            </div>
-                                            <button>View now</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        </div> */}
                     </div>
                 </section>
 
